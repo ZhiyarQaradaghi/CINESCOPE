28990ec47f291b9acfe47ce1325f0803f621fd29
Zhiyar_Qaradaghi <zhiyar.burhan2003@gmail.com>
2025-03-18 13:33:35 +0300

    Deployment

--- src/services/apiUtils.js
+++ src/services/apiUtils.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
+  import.meta.env.VITE_SOCKET_SERVER || "http://localhost:5000/api";
 
 export const makeRequest = async (endpoint, options = {}) => {
   try {
